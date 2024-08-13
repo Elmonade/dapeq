195365ee938a07e73ceee425ce9938530d0204f8
Lemon <bb222mf@student.lnu.se>
2024-08-13 16:10:22 +0200

    RED: Send a reference to a the sink. I think this is the way. Not sure how to actually implement it. Fiddled with bit of thread as well.

--- src/commands/audio_playback.rs
+++ src/commands/audio_playback.rs
@@ -1,25 +1,33 @@
 use std::fs::File;
 use std::io::BufReader;
+use std::thread;
 use rodio::{Decoder, OutputStream, Sink};
 
 pub fn play() {
-    println!("Received play command.");
+    let _ = thread::Builder::new()
+        .name("music_player".to_string())
+        .spawn(move || {
+            println!("Received play command.");
 
-    // _stream must live as long as the sink
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+            // _stream must live as long as the sink
+            let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+            let sink = Sink::try_new(&stream_handle).unwrap();
 
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open("file_example_OOG_2MG.ogg").unwrap());
-    // Decode that sound file into a source
-    let source = Decoder::new(file).unwrap();
-    sink.append(source);
+            // Load a sound from a file, using a path relative to Cargo.toml
+            let file =
+                BufReader::new(File::open("file_example_OOG_2MG.ogg").unwrap());
+            // Decode that sound file into a source
+            let source = Decoder::new(file).unwrap();
+            sink.append(source);
 
-    // The sound plays in a separate thread. This call will block the current thread until the sink
-    // has finished playing all its queued sounds.
-    sink.sleep_until_end();
+            // The sound plays in a separate thread.
+            // This call will block the current thread until the sink
+            // has finished playing all its queued sounds.
+            pause(&sink);
+            sink.sleep_until_end();
+    });
 }
 
-pub fn pause() {
-    // sink.pause(source);
+pub fn pause(sink: &Sink) {
+    sink.pause();
 }
